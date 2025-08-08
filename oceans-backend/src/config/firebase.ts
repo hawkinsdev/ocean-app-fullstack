468300e74a283cd063f4293479b3f5d71121694e
Jesus Manuel Varela Miranda <jesusmvarelamiranda@gmail.com>
2025-08-08 12:43:56 -0500

    deploy(docker): fix dockefile to key firebase

--- oceans-backend/src/config/firebase.ts
+++ oceans-backend/src/config/firebase.ts
@@ -1,5 +1,5 @@
-import admin from "firebase-admin";
-import serviceAccount from "./firebase-key.json";
+import admin from 'firebase-admin';
+import serviceAccount from './firebase-key.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
