02fc26beb828c050acaa0504c3c12b5a9c4a09a8
Jesus Manuel Varela Miranda <jesusmvarelamiranda@gmail.com>
2025-08-08 12:03:20 -0500

    deploy: config dockerfile deploy

--- oceans-frontend/src/api/axios.client.ts
+++ oceans-frontend/src/api/axios.client.ts
@@ -7,7 +7,7 @@ import axios, {
 
 const createApiClient = () => {
   const api: AxiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3001",
     timeout: 5000,
     headers: {
       "Content-Type": "application/json",
